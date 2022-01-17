fe6bd82210ec8f7293e304dd022a3bfff5153dc9
Rebecca Herrera <rebeccadherrera@gmail.com>
2022-01-17 16:52:33 -0500

    adding more to party id

--- server.js
+++ server.js
@@ -162,6 +162,35 @@ app.get('/api/parties', (req, res) => {
     });
   });
 
+// Update a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors) {
+        res.status(400).json({ error: errors });
+    }
+    
+    const sql = `UPDATE candidates SET party_id = ? 
+                 WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        // check if a record was found
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Candidate not found'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        });
+      }
+    });
+  });
+
 //Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
