57ab5522d8dd5d6f62ddcd2bd882df592c392b70
Rebecca Herrera <rebeccadherrera@gmail.com>
2022-01-18 20:30:15 -0500

    added password

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   // Your MySQL username,
   user: 'root',
   // Your MySQL password
-  password: '',
+  password: 'diSney96girL!321',
   database: 'election'
 });
 
